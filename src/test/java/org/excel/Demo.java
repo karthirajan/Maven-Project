421cc31905e00f90bc4efd44bde3c632b5cfde2b
ishwaryaorakash <ishwaryar27@gmail.com>
2022-07-09 10:27:13 +0530

    Today's task

--- src/test/java/org/excel/Demo.java
+++ src/test/java/org/excel/Demo.java
@@ -1,10 +1,14 @@
 package org.excel;
 
 public class Demo {
+	public void Name() {
+		System.out.println("my own branch");
+	}
 	
 	public static void main(String[] args) {
+		Demo a = new Demo();
+		a.Name();
 		
-		System.out.println("my own branch");
 		
 	}
 
--- src/test/java/org/excel/sample.java
+++ src/test/java/org/excel/sample.java
@@ -2,7 +2,9 @@ package org.excel;
 
 public class sample {
 	public static void main(String[] args) {
-		System.out.println("SAMPLE OUTPUT");
+		String s1 = "SAMPLE OUTPUT";
+		int m = s1.indexOf("A");
+		System.out.println(m);
 		
 	}
 
